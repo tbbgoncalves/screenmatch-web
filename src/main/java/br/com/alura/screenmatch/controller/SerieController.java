e5d8136ac6c49f72d4a47587a7a52246849411ae
Tiago Bastos <tbbgoncalves@outlook.com>
2024-02-03 03:32:21 -0300

    implementando a busca por id, para apresentar os detalhes de cada série no front

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos() {
         return serieService.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return serieService.obterPorId(id);
     }
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,6 +25,20 @@ public class SerieService {
 
     public List<SerieDTO> obterLancamentos() {
         return converteDados(serieRepository.encontrarEpisodiosMaisRecentes());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serieBuscada = serieRepository.findById(id);
+
+        if(serieBuscada.isPresent()) {
+            Serie s = serieBuscada.get();
+
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(),
+                    s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        else {
+            return null;
+        }
     }
 
     private List<SerieDTO> converteDados(List<Serie> series) {
